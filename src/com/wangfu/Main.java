94e7a86dbea1dafc4f768bf3adf22f650d391365
liutangchen <1253584040@qq.com>
2021-08-31 17:37:29 +0800

    大数相乘

--- src/com/wangfu/Main.java
+++ src/com/wangfu/Main.java
@@ -1,40 +1,9 @@
 package com.wangfu;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
+/**
+ * @Author: liutangchen
+ * @Date: 2021/5/8 14:57
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < t; i++) {
-            int n = in.nextInt();
-            for (int j = 0; j < n; j++) {
-                int[] arr = new int[2];
-                arr[0] = in.nextInt();
-                arr[1] = in.nextInt();
-                queue.offer(arr);
-            }
-            int[] res = new int[n];
-            int index = 0;
-            int currentTime = 1;
-            while (!queue.isEmpty()) {
-                int[] arr = queue.poll();
-                if (currentTime < arr[0])
-                    currentTime = arr[0];
-                if (currentTime >= arr[0] && currentTime <= arr[1]) {
-                    res[index++] = currentTime;
-                    currentTime++;
-                } else {
-                    res[index++] = 0;
-                }
-            }
-            for (int k = 0; k < n - 1; k++) {
-                System.out.print(res[k] + " ");
-            }
-            System.out.println(res[res.length - 1]);
-        }
-    }
+
 }
